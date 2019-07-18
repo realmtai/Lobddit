2690597a42021e3730e7ceb0f8781798090dfc71
Michael Tai <michael@downloadthebear.com>
2019-07-18 14:20:04 -0400

    - Able to fetch data from reddit api within a view controller

--- Lobddit/MainViewController.swift
+++ Lobddit/MainViewController.swift
@@ -9,11 +9,49 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    fileprivate let api = RedditAPI()
+    fileprivate let workQueue = DispatchQueue(label: "com.michael.tai.mainviewcontroller.workq")
+    
+    fileprivate var dataStore: [Article] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                
+            }
+        }
+    }
+    
+    
+    //MARK:- Life Cycle
+    //MARK:
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.workQueue.async { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.fetchData()
+        }
     }
+    
+    //MARK:- Actions
+    //MARK:
+    
+    func fetchData() {
+        api.getArticles(.swift) {[weak self] result in
+            self?.workQueue.async { [weak self] in
+                guard let strongSelf = self else { return }
+                switch result {
+                case .success(let articles):
+                    strongSelf.dataStore = articles
+                case .failure(let error):
+                    // Do something here
+                    break
+                }
+            }
+        }
+    }
+    
+
 
 
 }
