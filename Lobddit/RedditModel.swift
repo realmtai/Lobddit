93372e602306fcfd8309b86479049656115871c9
Michael Tai <michael@downloadthebear.com>
2019-07-18 09:25:20 -0400

    - Able to parse reddit API response

---
+++ Lobddit/RedditModel.swift
@@ -0,0 +1,63 @@
+//
+//  RedditModel.swift
+//  Lobddit
+//
+//  Created by PersonA on 7/18/19.
+//  Copyright © 2019 Michael Tai. All rights reserved.
+//
+
+import Foundation
+
+struct NewJSONResp: Codable {
+    let content: Listing?
+    
+    private enum CodingKeys: String, CodingKey {
+        case content = "data"
+    }
+    
+}
+
+struct Listing: Codable {
+    let count: Int
+    let children: [Child]
+    
+    private enum CodingKeys: String, CodingKey {
+        case count = "dist"
+        case children
+    }
+    
+}
+
+struct Child: Codable {
+    let kind: String?
+    let content: Article
+    
+    private enum CodingKeys: String, CodingKey {
+        case kind
+        case content = "data"
+    }
+}
+
+struct Article: Codable {
+    let url: URL?
+    let thumbnail: URL?
+    let thumbnailWidth: Double?
+    let thumbnailHeight: Double?
+    
+    private enum CodingKeys: String, CodingKey {
+        case url
+        case thumbnail
+        case thumbnailWidth = "thumbnail_width"
+        case thumbnailHeight = "thumbnail_height"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        url = try? values.decode(URL.self, forKey: .url)
+        thumbnail = try? values.decode(URL.self, forKey: .thumbnail)
+        thumbnailWidth = try? values.decode(Double.self, forKey: .thumbnailWidth)
+        thumbnailHeight = try? values.decode(Double.self, forKey: .thumbnailHeight)
+    }
+    
+}
+
--- LobdditTests/LobdditTests.swift
+++ LobdditTests/LobdditTests.swift
@@ -19,9 +19,55 @@ class LobdditTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDecodingArticle() {
+        let articleJSON =
+"""
+        {
+          "approved_at_utc": null,
+          "subreddit": "swift",
+          "selftext": "",
+          "author_fullname": "t2_28u4w6s7",
+          "downs": 0,
+          "thumbnail_height": 93,
+          "hide_score": false,
+          "thumbnail_width": 140,
+          "author_flair_template_id": null,
+          "is_original_content": false,
+          "user_reports": [
+          ],
+          "thumbnail": "https://b.thumbs.redditmedia.com/YzrK1u42GX-JLFesQ8Sxv_vmIiP5gPNkVpf7eQw5zSo.jpg",
+          "edited": false,
+          "author_patreon_flair": false,
+          "author_flair_text_color": null,
+          "permalink": "/r/swift/comments/ceq95k/we_are_launching_a_community_with_a_project/",
+          "parent_whitelist_status": "all_ads",
+          "stickied": false,
+          "url": "https://i.redd.it/gr6p4lb471b31.png",
+          "subreddit_subscribers": 48881,
+          "created_utc": 1563442340.0,
+          "discussion_type": null,
+          "media": null,
+          "is_video": false
+        }
+""".data(using: .utf8)!
+        guard let article = try? JSONDecoder().decode(Article.self, from: articleJSON) else {
+            XCTAssertTrue(false, "Unable to decode articleJSON")
+            return
+        }
+        XCTAssertEqual(article.thumbnailWidth!, 140)
+        XCTAssertEqual(article.thumbnailHeight!, 93)
+        XCTAssertEqual(article.thumbnail!.absoluteString, "https://b.thumbs.redditmedia.com/YzrK1u42GX-JLFesQ8Sxv_vmIiP5gPNkVpf7eQw5zSo.jpg")
+        XCTAssertEqual(article.url!.absoluteString, "https://i.redd.it/gr6p4lb471b31.png")
+    }
+    
+    func testDecodingRSwiftNew() {
+        let jsonURL = Bundle(for: type(of: self)).url(forResource: "testDataNormal", withExtension: "json")!
+        let rswiftRespData = try! Data(contentsOf: jsonURL)
+        guard let rswiftRespJSON = try? JSONDecoder().decode(NewJSONResp.self, from: rswiftRespData) else {
+            XCTAssertTrue(false, "Unable to decode articleJSON")
+            return
+        }
+        XCTAssertNotNil(rswiftRespJSON.content)
     }
 
     func testPerformanceExample() {
