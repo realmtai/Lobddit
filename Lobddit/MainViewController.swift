155a7d480db6de6064bdcb58d6ab69144ac72f9e
Michael Tai <michael@downloadthebear.com>
2019-07-18 17:06:59 -0400

    - more UI cleanup with CHTCollectionViewWaterfallLayout

--- Lobddit/MainViewController.swift
+++ Lobddit/MainViewController.swift
@@ -16,7 +16,10 @@ class MainCollectionCell: UICollectionViewCell {
     
     var text: String {
         get { return textLabel.text ?? "" }
-        set(newValue) { textLabel.text = newValue }
+        set(newValue) {
+            textLabel.text = newValue
+            textLabel.sizeToFit()
+        }
     }
     
     var imageURL: URL? {
@@ -28,7 +31,7 @@ class MainCollectionCell: UICollectionViewCell {
             }
         }
     }
-    
+
     override func prepareForReuse() {
         thumbnailImage.hnk_cancelSetImage()
         super.prepareForReuse()
@@ -45,10 +48,15 @@ class MainViewController: UIViewController {
         didSet { DispatchQueue.main.async { self.mainCollectionView.reloadData() } }
     }
     
+    private let collctionViewMagicConstant: CGFloat = 140
+    
     //MARK:- UI Elements
     //MARK:
 
-    @IBOutlet weak var mainCollectionView: UICollectionView!
+    @IBOutlet weak var mainCollectionView: UICollectionView! {
+        didSet {
+        }
+    }
     
     
     //MARK:- Life Cycle
@@ -56,6 +64,8 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainCollectionView.collectionViewLayout = collectionViewLayout(forSize: view.frame.size)
+        mainCollectionView.collectionViewLayout.invalidateLayout()
         self.workQueue.async { [weak self] in
             guard let strongSelf = self else { return }
             strongSelf.fetchData()
@@ -66,6 +76,12 @@ class MainViewController: UIViewController {
         if let destVC = segue.destination as? ArticleViewController, let datum = sender as? Article {
             destVC.article = datum
         }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        mainCollectionView.collectionViewLayout = collectionViewLayout(forSize: size)
+        mainCollectionView.collectionViewLayout.invalidateLayout()
     }
     
     //MARK:- Actions
@@ -89,11 +105,18 @@ class MainViewController: UIViewController {
     func showDetail(for article: Article) {
         performSegue(withIdentifier: "gotoArticleViewController", sender: article)
     }
+    
+    func collectionViewLayout(forSize size: CGSize) -> CHTCollectionViewWaterfallLayout {
+        let layout = CHTCollectionViewWaterfallLayout()
+        let width = size.width
+        layout.columnCount = Int(width/collctionViewMagicConstant)
+        return layout
+    }
 
 }
 
 
-extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, CHTCollectionViewDelegateWaterfallLayout {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let datum = dataStore[indexPath.row]
@@ -113,5 +136,13 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
         cell.imageURL = datum.thumbnail
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let datum = dataStore[indexPath.row]
+        let text = datum.title ?? ""
+        let height = text.height(withConstrainedWidth: collctionViewMagicConstant, font: UIFont.systemFont(ofSize: 18, weight: .regular))
+        return CGSize(width: collctionViewMagicConstant, height: height)
+    }
+    
     
 }
--- Lobddit/SystemFrameworkExtenstion.swift
+++ Lobddit/SystemFrameworkExtenstion.swift
@@ -13,3 +13,20 @@ extension URL {
         get { return (scheme ?? "") == "https" }
     }
 }
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return ceil(boundingBox.width)
+    }
+}
+
